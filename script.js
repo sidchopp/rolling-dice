d3e66e093391e3d975250e1b2da5bbff917d4b43
Sid <sidchopp@gmail.com>
2021-08-01 15:54:41 -0600

    Defining the 1st look of the App

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+// a better way to get select element by id is:
+//const score2 = document.getElementById('score--1').textContent = 0;
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
+
