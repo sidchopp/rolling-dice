d4ae04dbc4a77afe938ceb94a515d910435038c5
Sid <sidchopp@gmail.com>
2021-08-02 15:39:12 -0600

    switching of player functionality included

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Selecting Elements
+// Selecting Elements..
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
@@ -13,15 +13,19 @@ const btnHold = document.querySelector('.btn--hold');
 // a better way to get select element by id is:
 //const score2 = document.getElementById('score--1').textContent = 0;
 
-// Starting conditions
+// Starting conditions..
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-
+// we define an array for the total scores the first zero is for 1st player and 2nd zero is for 2nd player
+const scores = [0, 0];
+// to select which player is the active player. 
+let activePlayer = 0;
 // to store the initial score 
 let currentScore = 0;
 
-// Rolling dice functionalty
+
+// Rolling dice functionalty..
 
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -35,11 +39,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice value to current socre
     currentScore += dice;
-    current0El.textContent = currentScore; // change later
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
-    // switch to next player
-    document.querySelector('.player--0').classList.remove('player--active');
-    document.querySelector('.player--1').classList.add('player--active');
+    // switch to next player. 
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    console.log(activePlayer);
+    currentScore = 0;
+
   }
 
 
