da128b02394d776699950ad97aea5a3526e5c932
Sid <sidchopp@gmail.com>
2021-08-06 16:03:41 -0600

    header removed

--- script.js
+++ script.js
@@ -106,6 +106,37 @@ btnHold.addEventListener('click', function () {
 btnNew.addEventListener('click', init);
 
 
+// For Modal
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+
+btnsOpenModal.addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
 
 
 
